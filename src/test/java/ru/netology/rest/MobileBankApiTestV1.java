b7c55aa10efac5a1cc58b34e23c4ebbf9b531c56
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-05-27 11:57:45 +0300

    .appveyor.yml added

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,40 +1,37 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.RestAssured.given;
-import  io.restassured.http.ContentType;
+
+import io.restassured.http.ContentType;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
 
-      ;
-    }
-    @Test
-    void shouldReturnDemoAccountsCurrency() {
-        // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
-                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
+        ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsCurrency() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
                 .then()
                 .body("[0].currency", equalTo("RUB"))
         ;
-}
+    }
 }
